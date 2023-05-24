5945af3fad00e23d65c6c6a503f5429d22bdd989
Sterling <sterlingbarton81@gmail.com>
2023-05-24 16:16:27 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -15,9 +15,11 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
 
 @app.route('/games')
 def games():
@@ -39,10 +41,11 @@ def games():
 
     return response
 
+
 @app.route('/games/<int:id>')
 def game_by_id(id):
     game = Game.query.filter(Game.id == id).first()
-    
+
     game_dict = game.to_dict()
 
     response = make_response(
@@ -52,20 +55,41 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
 
-    return response
+        return response
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.form.get("score"),
+            comment=request.form.get("comment"),
+            game_id=request.form.get("game_id"),
+            user_id=request.form.get("user_id")
+        )
+
+        db.session.add(new_review)
+        db.session.commit()
+
+        review_dict = new_review.to_dict()
+
+        response = make_response(
+            review_dict,
+            201
+        )
+        return response
+
 
 @app.route('/users')
 def users():
@@ -81,6 +105,52 @@ def users():
     )
 
     return response
+
+
+@app.route('/reviews/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
+
+    if request.method == 'GET':
+        review_dict = review.to_dict()
+
+        response = make_response(
+            review_dict,
+            200
+        )
+        return response
+
+    elif request.method == 'PATCH':
+        review = Review.query.filter(Review.id == id).first()
+
+        for attr in request.form:
+            setattr(review, attr, request.form.get(attr))
+
+        db.session.add(review)
+        db.session.commit()
+
+        review_dict = review.to_dict()
+
+        response = make_response(
+            review_dict,
+            200
+        )
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted successfully."
+        }
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
